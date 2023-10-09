4e03f581c23f18c4c1609f60a9fffbb3bb4ae218
PauLOk777 <trocjuk2000@gmail.com>
2023-10-09 16:07:17 +0300

    Remove validation experiments number from API contract

--- src/main/java/ua/kpi/ip22mp/trotsiuk/mmcmlr/models/ModifiedMethodOfCmlrRequestBody.java
+++ src/main/java/ua/kpi/ip22mp/trotsiuk/mmcmlr/models/ModifiedMethodOfCmlrRequestBody.java
@@ -1,5 +1,5 @@
 package ua.kpi.ip22mp.trotsiuk.mmcmlr.models;
 
 public record ModifiedMethodOfCmlrRequestBody(
-        int experimentsNumber, int initialExperimentsNumber, int validationExperimentsNumber,
+        int experimentsNumber, int initialExperimentsNumber,
         double[][] independentVariables, double[] correctCoefficients, double[][] errors) {}
