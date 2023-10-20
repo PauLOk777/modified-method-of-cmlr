db5d33115c5f2c189806d80f96e5a5797eb2e9cf
PauLOk777 <trocjuk2000@gmail.com>
2023-10-20 21:40:16 +0300

    Fix NotEmptyMatrix annotation

--- src/main/java/ua/kpi/ip22mp/trotsiuk/mmcmlr/validation/constraints/NotEmptyMatrix.java
+++ src/main/java/ua/kpi/ip22mp/trotsiuk/mmcmlr/validation/constraints/NotEmptyMatrix.java
@@ -2,7 +2,7 @@ package ua.kpi.ip22mp.trotsiuk.mmcmlr.validation.constraints;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-import ua.kpi.ip22mp.trotsiuk.mmcmlr.validation.validators.IntRealRangeValidator;
+import ua.kpi.ip22mp.trotsiuk.mmcmlr.validation.validators.NotEmptyMatrixValidator;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
@@ -13,7 +13,7 @@ import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
-@Constraint(validatedBy = IntRealRangeValidator.class)
+@Constraint(validatedBy = NotEmptyMatrixValidator.class)
 @Target({ FIELD, PARAMETER })
 @Retention(RUNTIME)
 public @interface NotEmptyMatrix {
