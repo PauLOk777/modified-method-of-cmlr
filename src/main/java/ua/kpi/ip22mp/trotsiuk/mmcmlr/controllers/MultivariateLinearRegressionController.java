a135960c719ba62d43fabf924834cef72dbda925
PauLOk777 <trocjuk2000@gmail.com>
2023-10-23 21:21:40 +0300

    Fix issue with mmclr validator

--- src/main/java/ua/kpi/ip22mp/trotsiuk/mmcmlr/controllers/MultivariateLinearRegressionController.java
+++ src/main/java/ua/kpi/ip22mp/trotsiuk/mmcmlr/controllers/MultivariateLinearRegressionController.java
@@ -2,12 +2,17 @@ package ua.kpi.ip22mp.trotsiuk.mmcmlr.controllers;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody;
 import ua.kpi.ip22mp.trotsiuk.mmcmlr.services.MultivariateLinearRegressionService;
+import ua.kpi.ip22mp.trotsiuk.mmcmlr.validation.validators.ModifiedMethodOfCmlrRequestBodyValidator;
 
 @RestController
 @RequestMapping("/multivariate-linear-regression")
@@ -17,10 +22,19 @@ public class MultivariateLinearRegressionController {
     private static final String MODIFIED_METHOD_OF_CMLR_PAGE_WITH_IV = "modifiedMethodOfCmlrWithIv";
 
     private final MultivariateLinearRegressionService multivariateLinearRegressionService;
+    private final Validator modifiedMethodOfCmlrRequestBodyValidator;
 
     @Autowired
-    public MultivariateLinearRegressionController(MultivariateLinearRegressionService multivariateLinearRegressionService) {
+    public MultivariateLinearRegressionController(
+            MultivariateLinearRegressionService multivariateLinearRegressionService,
+            @Qualifier("modifiedMethodOfCmlrRequestBodyValidator") Validator modifiedMethodOfCmlrRequestBodyValidator) {
         this.multivariateLinearRegressionService = multivariateLinearRegressionService;
+        this.modifiedMethodOfCmlrRequestBodyValidator = modifiedMethodOfCmlrRequestBodyValidator;
+    }
+
+    @InitBinder
+    private void initBinder(WebDataBinder binder) {
+        binder.setValidator(modifiedMethodOfCmlrRequestBodyValidator);
     }
 
     @PostMapping("/modified-method-of-cmlr")
