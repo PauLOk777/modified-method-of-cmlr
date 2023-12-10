1ecbcb621da6c1b7ab193832bc0b9572eaaa3f1c
PauLOk777 <trocjuk2000@gmail.com>
2023-12-10 21:46:54 +0200

    Align validation limits

--- src/main/java/ua/kpi/ip22mp/trotsiuk/mmcmlr/requests/ModifiedMethodOfCmlrRequestBody.java
+++ src/main/java/ua/kpi/ip22mp/trotsiuk/mmcmlr/requests/ModifiedMethodOfCmlrRequestBody.java
@@ -9,18 +9,18 @@ import ua.kpi.ip22mp.trotsiuk.mmcmlr.validation.constraints.RectangularMatrix;
 
 public record ModifiedMethodOfCmlrRequestBody(
         @Min(value = 1, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.repetitionsNumberOfActiveExperiments.Min}")
-        @Max(value = 100, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.repetitionsNumberOfActiveExperiments.Max}")
+        @Max(value = 10, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.repetitionsNumberOfActiveExperiments.Max}")
         int repetitionsNumberOfActiveExperiments,
         @Min(value = 1, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.numberOfValidationSequences.Min}")
-        @Max(value = 100, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.numberOfValidationSequences.Max}")
+        @Max(value = 10, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.numberOfValidationSequences.Max}")
         int numberOfValidationSequences,
-        @Size(min = 1, max = 101, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.correctCoefficients.Size}")
+        @Size(min = 1, max = 31, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.correctCoefficients.Size}")
         double[] correctCoefficients,
         @NotEmptyMatrix(message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.independentVariables.NotEmptyMatrix}")
         @RectangularMatrix(message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.independentVariables.RectangularMatrix}")
-        @MatrixSize(maxRows = 100, maxColumns = 100, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.independentVariables.MatrixSize}")
+        @MatrixSize(maxRows = 300, maxColumns = 30, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.independentVariables.MatrixSize}")
         double[][] independentVariables,
         @NotEmptyMatrix(message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.errors.NotEmptyMatrix}")
         @RectangularMatrix(message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.errors.RectangularMatrix}")
-        @MatrixSize(maxRows = 100, maxColumns = 100, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.errors.MatrixSize}")
+        @MatrixSize(maxRows = 300, maxColumns = 20, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.errors.MatrixSize}")
         double[][] errors) {}
--- src/main/java/ua/kpi/ip22mp/trotsiuk/mmcmlr/requests/NormallyDistributedRandomNumbersMMCMLRRequestBody.java
+++ src/main/java/ua/kpi/ip22mp/trotsiuk/mmcmlr/requests/NormallyDistributedRandomNumbersMMCMLRRequestBody.java
@@ -9,18 +9,19 @@ import ua.kpi.ip22mp.trotsiuk.mmcmlr.validation.constraints.RectangularMatrix;
 
 public record NormallyDistributedRandomNumbersMMCMLRRequestBody (
     @Min(value = 1, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.repetitionsNumberOfActiveExperiments.Min}")
-    @Max(value = 100, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.repetitionsNumberOfActiveExperiments.Max}")
+    @Max(value = 10, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.repetitionsNumberOfActiveExperiments.Max}")
     int repetitionsNumberOfActiveExperiments,
     @Min(value = 1, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.numberOfValidationSequences.Min}")
-    @Max(value = 100, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.numberOfValidationSequences.Max}")
+    @Max(value = 10, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.numberOfValidationSequences.Max}")
     int numberOfValidationSequences,
-    @Min(value = 1, message = "{}")
+    @Min(value = 1, message = "{number.of.runs.Min}")
+    @Min(value = 10000, message = "{number.of.runs.Max}")
     int numberOfRuns,
-    @Size(min = 1, max = 101, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.correctCoefficients.Size}")
+    @Size(min = 1, max = 31, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.correctCoefficients.Size}")
     double[] correctCoefficients,
     @NotEmptyMatrix(message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.independentVariables.NotEmptyMatrix}")
     @RectangularMatrix(message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.independentVariables.RectangularMatrix}")
-    @MatrixSize(maxRows = 100, maxColumns = 100, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.independentVariables.MatrixSize}")
+    @MatrixSize(maxRows = 300, maxColumns = 30, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.ModifiedMethodOfCmlrRequestBody.independentVariables.MatrixSize}")
     double[][] independentVariables,
     double mean,
     @Min(value = 0, message = "{ua.kpi.ip22mp.trotsiuk.mmcmlr.requests.MatrixOfNormallyDistributedRandomNumbersRequestBody.stdDev.Min}")
